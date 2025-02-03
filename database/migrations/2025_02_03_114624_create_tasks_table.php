d46ce65b937643919c6044aaf2e5b34087150b45
mirzababarhussain <48199676+mirzababarhussain@users.noreply.github.com>
2025-02-03 16:58:04 +0500

    commit DB Detail

---
+++ database/migrations/2025_02_03_114624_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->longText('description')->nullable();
+            $table->string('status')->default('pending');
+            $table->foreignId('project_id')->constrained()->onDelete('cascade');
+            $table->integer('created_by');
+            $table->integer('assigned_to');
+            $table->dateTime('due_date');
+            $table->dateTime('completed_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
